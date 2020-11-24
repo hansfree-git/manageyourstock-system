8a8e7f45f56a656db9fd73f4d58c574e2be62225
hansfree <kobehans1@gmail.com>
2020-11-24 15:48:25 +0000

    secret key

--- stock_mgt/settings.py
+++ stock_mgt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY='bf28fb59e9efb711e3fe01a936e90c1c78399d772bf4dca5'
+SECRET_KEY=config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
